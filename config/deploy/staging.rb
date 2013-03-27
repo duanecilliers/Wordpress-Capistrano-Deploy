1cd65c79df47d254a02394df2292d5bb00032a34
Duane Cilliers <duanecilliers@gmail.com>
2013-03-27 17:07:14 +0200

    Small cleanup, remove password var and changed tft_staging: to my_project:

--- config/deploy/staging.rb
+++ config/deploy/staging.rb
@@ -15,7 +15,6 @@ server "#{user}@#{domain}", :app #This line doesn't need to change
 
 set :group, "group" #TODO
 
-
 # We'll be referencing this when we go to search and replace the database, this
 # is where we'll clone the search and replace script if it doesn't exist
 set :local_path, '~/Sites/localhost' #TODO
@@ -53,118 +52,117 @@ set :blogs_dir_path, "/path/to/wp-content/blogs.dir" #TODO
 
 #Symlink wp-config to the release folder so it doesn't get overwritten
 namespace :my_project do
-    task :symlink, :roles => :app do
-        run "ln -nfs #{shared_path}/wp-config-staging.php #{release_path}/wp-config-staging.php"
-        run "ln -nfs #{shared_path}/.htaccess #{release_path}/.htaccess"
-    end
+	task :symlink, :roles => :app do
+		run "ln -nfs #{shared_path}/wp-config-staging.php #{release_path}/wp-config-staging.php"
+		run "ln -nfs #{shared_path}/.htaccess #{release_path}/.htaccess"
+	end
 end
 
 after "deploy:finalize_update", "staging:symlink"
 
 #Create backups directory
 namespace(:deploy) do
-  desc "Add backups directory"
-  task :add_backups_dir, :roles => :app do
-    run "mkdir #{shared_path}/backups"
-  end
+	desc "Add backups directory"
+	task :add_backups_dir, :roles => :app do
+		run "mkdir #{shared_path}/backups"
+	end
 end
 after "deploy:setup", "deploy:add_backups_dir"
 
 #Backup Remote DB
 namespace(:deploy) do
-  desc "Backup MySQL Database"
-  task :mysqlbackup, :roles => :app do
-    run "mysqldump -u#{remote_db_username} -p#{remote_db_password} #{remote_db_database} > #{shared_path}/backups/#{release_name}.sql"
-  end
+	desc "Backup MySQL Database"
+	task :mysqlbackup, :roles => :app do
+		run "mysqldump -u#{remote_db_username} -p#{remote_db_password} #{remote_db_database} > #{shared_path}/backups/#{release_name}.sql"
+	end
 end
 before "deploy:create_symlink", "deploy:mysqlbackup"
 
 #Restore Remote DB
 namespace(:deploy) do
-  desc "Restore MySQL Database"
-  task :mysqlrestore, :roles => :app do
-    backups = capture("ls -1 #{shared_path}/backups/").split("\n")
-    default_backup = backups.last
-    puts "Available backups: "
-    puts backups
-    backup = Capistrano::CLI.ui.ask "Which backup would you like to restore? [#{default_backup}] "
-    backup_file = default_backup if backup.empty?
+	desc "Restore MySQL Database"
+	task :mysqlrestore, :roles => :app do
+		backups = capture("ls -1 #{shared_path}/backups/").split("\n")
+		default_backup = backups.last
+		puts "Available backups: "
+		puts backups
+		backup = Capistrano::CLI.ui.ask "Which backup would you like to restore? [#{default_backup}] "
+		backup_file = default_backup if backup.empty?
 
-    run "mysql -u#{remote_db_username} -p#{remote_db_password} #{remote_db_database} < #{shared_path}/backups/#{backup_file}"
-  end
+		run "mysql -u#{remote_db_username} -p#{remote_db_password} #{remote_db_database} < #{shared_path}/backups/#{backup_file}"
+	end
 end
 
 #Dump Local DB, replace Remote DB, update urls in DB
 namespace(:deploy) do
-  desc "Dump Local DB and replace Remote DB"
-  task :dbupdate, :roles => :app do
-    run_locally "mysqldump -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} -C -c --skip-add-locks #{local_db_database} | ssh #{user}@#{domain} -p #{port} 'mysql -u #{remote_db_username} -p#{remote_db_password} #{remote_db_database}'"
-    run "[ -d #{shared_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{shared_path}/srdb"
-    run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_url} -r #{remote_site_url} guid=0"
-    #run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_path} -r #{remote_site_path} guid=0"
-  end
+	desc "Dump Local DB and replace Remote DB"
+	task :dbupdate, :roles => :app do
+		run_locally "mysqldump -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} -C -c --skip-add-locks #{local_db_database} | ssh #{user}@#{domain} -p #{port} 'mysql -u #{remote_db_username} -p#{remote_db_password} #{remote_db_database}'"
+		run "[ -d #{shared_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{shared_path}/srdb"
+		run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_url} -r #{remote_site_url} guid=0"
+		#run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_path} -r #{remote_site_path} guid=0"
+	end
 end
 
 #Dump Local DB, replace Remote DB, update urls in DB
 namespace(:deploy) do
-  desc "Dump Local DB and replace Remote DB"
-  task :mu_update, :roles => :app do
-    run_locally "mysqldump -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} -C -c --skip-add-locks #{local_db_database} --ignore-table=#{dbp}blogs --ignore-table=#{dbp}blog_versions --ignore_table=#{dbp}site --ignore-table=#{dbp}sitemeta | ssh #{user}@#{domain} -p #{port} 'mysql -u #{remote_db_username} -p#{remote_db_password} #{remote_db_database}'"
-    run "[ -d #{shared_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{shared_path}/srdb"
-    run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_url} -r #{remote_site_url} guid=0"
-  end
+	desc "Dump Local DB and replace Remote DB"
+	task :mu_update, :roles => :app do
+		run_locally "mysqldump -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} -C -c --skip-add-locks #{local_db_database} --ignore-table=#{dbp}blogs --ignore-table=#{dbp}blog_versions --ignore_table=#{dbp}site --ignore-table=#{dbp}sitemeta | ssh #{user}@#{domain} -p #{port} 'mysql -u #{remote_db_username} -p#{remote_db_password} #{remote_db_database}'"
+		run "[ -d #{shared_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{shared_path}/srdb"
+		run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_url} -r #{remote_site_url} guid=0"
+	end
 end
 
 #Dump Local DB, replace Remote DB, update urls in DB
 namespace(:deploy) do
-  desc "Dump Local DB and replace Remote DB"
-  task :local_mu_update, :roles => :app do
-    run_locally "ssh #{user}@#{domain} -p #{port} 'mysqldump -u#{remote_db_username} -p#{remote_db_password} -h#{remote_db_host} #{remote_db_database} -C -c --skip-add-locks --ignore-table=#{dbp}blogs --ignore-table=#{dbp}blog_versions --ignore_table=#{dbp}site --ignore-table=#{dbp}sitemeta' | mysql -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} #{local_db_database}"
-    run "[ -d #{local_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{local_path}/srdb"
-    run_locally "php -f #{local_path}/srdb/searchreplacedb2cli.php cli -h #{local_db_host} -d #{local_db_database} -u #{local_db_username} -p #{local_db_password} -c #{local_db_charset} -s #{remote_site_url} -r #{local_site_url} guid=0"
-  end
+	desc "Dump Local DB and replace Remote DB"
+	task :local_mu_update, :roles => :app do
+		run_locally "ssh #{user}@#{domain} -p #{port} 'mysqldump -u#{remote_db_username} -p#{remote_db_password} -h#{remote_db_host} #{remote_db_database} -C -c --skip-add-locks --ignore-table=#{dbp}blogs --ignore-table=#{dbp}blog_versions --ignore_table=#{dbp}site --ignore-table=#{dbp}sitemeta' | mysql -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} #{local_db_database}"
+		run "[ -d #{local_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{local_path}/srdb"
+		run_locally "php -f #{local_path}/srdb/searchreplacedb2cli.php cli -h #{local_db_host} -d #{local_db_database} -u #{local_db_username} -p #{local_db_password} -c #{local_db_charset} -s #{remote_site_url} -r #{local_site_url} guid=0"
+	end
 end
 
 #Dump Local DB, replace Remote DB, update urls in DB
 namespace(:deploy) do
-  desc "Dump Local DB and replace Remote DB"
-  task :update_posts, :roles => :app do
-    run_locally "mysqldump -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} -C -c --skip-add-locks #{local_db_database} #{db_prefix}posts #{db_prefix}postmeta #{db_prefix}options | ssh #{user}@#{domain} -p #{port} 'mysql -u #{remote_db_username} -p#{remote_db_password} #{remote_db_database}'"
-    run "[ -d #{shared_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{shared_path}/srdb"
-    run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_url} -r #{remote_site_url} guid=0"
-  end
+	desc "Dump Local DB and replace Remote DB"
+	task :update_posts, :roles => :app do
+		run_locally "mysqldump -u#{local_db_username} -p#{local_db_password} -h#{local_db_host} -C -c --skip-add-locks #{local_db_database} #{db_prefix}posts #{db_prefix}postmeta #{db_prefix}options | ssh #{user}@#{domain} -p #{port} 'mysql -u #{remote_db_username} -p#{remote_db_password} #{remote_db_database}'"
+		run "[ -d #{shared_path}/srdb/ ] || git clone git://github.com/interconnectit/Search-Replace-DB.git #{shared_path}/srdb"
+		run "php -f #{shared_path}/srdb/searchreplacedb2cli.php cli -h #{remote_db_host} -d #{remote_db_database} -u #{remote_db_username} -p #{remote_db_password} -c #{remote_db_charset} -s #{local_site_url} -r #{remote_site_url} guid=0"
+	end
 end
 
 #after "deploy:mysqlbackup", "deploy:dbupdate"
 
 #Sync uploads folder
 namespace(:deploy) do
-  desc "Sync Uploads folder"
-  task :sync_uploads, :roles => :app do
-    run_locally "if [ -d #{uploads_path} ]; then rsync -avhu #{uploads_path} -e 'ssh -p #{port}' #{user}@#{domain}:#{shared_path}; fi"
-  end
+	desc "Sync Uploads folder"
+	task :sync_uploads, :roles => :app do
+		run_locally "if [ -d #{uploads_path} ]; then rsync -avhu #{uploads_path} -e 'ssh -p #{port}' #{user}@#{domain}:#{shared_path}; fi"
+	end
 end
 
-after "tft_staging:symlink", "deploy:sync_uploads"
-
+after "my_project:symlink", "deploy:sync_uploads"
 
 #Sync blogs.dir folder
 namespace(:deploy) do
-  desc "Sync blogs.dir folder"
-  task :sync_blogs_dir, :roles => :app do
-    run_locally "if [ -d #{blogs_dir_path} ]; then rsync -avhu #{blogs_dir_path} -e 'ssh -p #{port}' #{user}@#{domain}:#{shared_path}; fi"
-  end
+	desc "Sync blogs.dir folder"
+	task :sync_blogs_dir, :roles => :app do
+		run_locally "if [ -d #{blogs_dir_path} ]; then rsync -avhu #{blogs_dir_path} -e 'ssh -p #{port}' #{user}@#{domain}:#{shared_path}; fi"
+	end
 end
-after "tft_staging:symlink", "deploy:sync_blogs_dir"
+after "my_project:symlink", "deploy:sync_blogs_dir"
 
 #Dump Local DB, replace Remote DB, update urls in DB
 namespace(:deploy) do
-  desc "Fix file permissions"
-  task :update_permissions, :roles => :app do
-    run "cd #{release_path} && chown -R #{user}:#{group} . && find . -type d -print0 | xargs -0 chmod 755"
-    run "cd #{shared_path}/uploads && chown -R #{user}:#{group} ."
-    run "cd #{shared_path}/blogs.dir && chown -R #{user}:#{group} ."
-  end
+	desc "Fix file permissions"
+	task :update_permissions, :roles => :app do
+		run "cd #{release_path} && chown -R #{user}:#{group} . && find . -type d -print0 | xargs -0 chmod 755"
+		run "cd #{shared_path}/uploads && chown -R #{user}:#{group} ."
+		run "cd #{shared_path}/blogs.dir && chown -R #{user}:#{group} ."
+	end
 end
 
-#after "tft_staging:symlink", "deploy:update_permissions"
+#after "my_project:symlink", "deploy:update_permissions"
